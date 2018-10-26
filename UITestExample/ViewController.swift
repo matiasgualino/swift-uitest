e5c97ed792c36e6e69db68350217376eb2691149
Matias Gualino <matias.gualino@brubank.com>
2018-10-26 10:29:31 -0300

    Initial Commit

---
+++ UITestExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITestExample
+//
+//  Created by Matias Gualino on 26/10/18.
+//  Copyright © 2018 Matias Gualino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
